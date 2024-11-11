2e28808ed885c164c04f4dafbd8a9a81434bdda7
bartekswiech <bartekswiech1103@gmail.com>
2024-11-11 10:25:25 -0700

    commmitting new changes again!

--- lua/bswiech/plugins/harpoon.lua
+++ lua/bswiech/plugins/harpoon.lua
@@ -6,7 +6,7 @@ return {
 	config = function()
 		local harpoon = require("harpoon")
 
-		print("We are testing this different main branch new branch")
+		print("now we areWe are testing this different main branch new branch")
 
 		harpoon:setup()
 		local function map(lhs, rhs, opts)
