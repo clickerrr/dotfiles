9aceeed9d48f5ee9623784d1e454d1b6eb760f09
bartekswiech <bartekswiech1103@gmail.com>
2024-12-16 20:49:18 -0700

    adjust formatting in nvim tree

--- lua/bswiech/plugins/nvim-tree.lua
+++ lua/bswiech/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function() 
+	config = function()
 		local nvimtree = require("nvim-tree")
 
 		vim.g.loaded_netrw = 1
@@ -11,7 +11,6 @@ return {
 			view = {
 				width = 35,
 				relativeNumber = true,
-				
 			},
 			renderer = {
 				indent_markers = {
@@ -22,30 +21,33 @@ return {
 						folder = {
 							arrow_closed = "→",
 							arrow_open = "↓",
-						}
-					}
-				}	
-
+						},
+					},
+				},
 			},
 			actions = {
 				open_file = {
 					window_picker = {
 						enable = false,
-					}
-				}
+					},
+				},
 			},
 			git = {
 				ignore = false,
-			}
+			},
 		})
 
-	local keymap = vim.keymap
+		local keymap = vim.keymap
 
-	keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", {desc = "Toggle file explorer"})
-	keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", {desc = "Toggle file explorer on current file"})
-	keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", {desc = "Collapse file explorer"})
-	keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", {desc = "Refresh file explorer"})
-
-	end
-
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		keymap.set(
+			"n",
+			"<leader>ef",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			{ desc = "Toggle file explorer on current file" }
+		)
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+		keymap.set("n", "<leader>es", "<C-]>", { desc = "Set as new file tree root" })
+	end,
 }
